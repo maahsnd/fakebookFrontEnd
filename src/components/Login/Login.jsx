859815fe5c9761acf5a39a04aa8b73e36dc84fca
mrwindupbirdesq <nmaahs11@gmail.com>
2023-11-01 12:42:29 -0700

    Serve login link

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -3,18 +3,7 @@ import React from 'react';
 function Login() {
   return (
     <div>
-      <button
-        onClick={async () => {
-          try {
-            const response = await fetch(
-              'http://localhost:3000/login/facebook'
-            );
-            console.log(response);
-          } catch (err) {
-            console.error(err);
-          }
-        }}
-      ></button>
+      <a href="https://localhost:3000/login/facebook">login</a>
     </div>
   );
 }
