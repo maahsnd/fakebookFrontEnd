9a8649b95cb965ab4117bab54729b2ac83e58612
mrwindupbirdesq <nmaahs11@gmail.com>
2023-11-03 16:53:53 -0700

    Add friends list title

--- src/components/FriendsList/FriendsList.jsx
+++ src/components/FriendsList/FriendsList.jsx
@@ -2,7 +2,11 @@ import React, { useEffect, useState } from 'react';
 import styles from './friendslist.module.css';
 
 function FriendsList() {
-  return <div className={styles.container}></div>;
+  return (
+    <div className={styles.container}>
+      <h3 className={styles.friendsTitle}>Contacts</h3>
+    </div>
+  );
 }
 
 export default FriendsList;
