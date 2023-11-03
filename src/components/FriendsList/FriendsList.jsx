a5c9d0c1f1668e112f3156b39525e720601df3a7
mrwindupbirdesq <nmaahs11@gmail.com>
2023-11-03 16:51:19 -0700

    Add friends list Background colro cyan for visualizing layout.

---
+++ src/components/FriendsList/FriendsList.jsx
@@ -0,0 +1,8 @@
+import React, { useEffect, useState } from 'react';
+import styles from './friendslist.module.css';
+
+function FriendsList() {
+  return <div className={styles.container}></div>;
+}
+
+export default FriendsList;
--- src/components/Timeline/Timeline.jsx
+++ src/components/Timeline/Timeline.jsx
@@ -1,8 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import styles from './timeline.module.css';
+import FriendsList from '../FriendsList/FriendsList';
 
 function Timeline() {
-  return <div className={styles.container}></div>;
+  return (
+    <div className={styles.container}>
+      <FriendsList />
+    </div>
+  );
 }
 
 export default Timeline;
