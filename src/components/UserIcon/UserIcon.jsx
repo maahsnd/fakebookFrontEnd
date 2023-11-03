d3d5e80e9da2d2fa3300e07e77bbdc33fdf83af8
mrwindupbirdesq <nmaahs11@gmail.com>
2023-11-03 16:18:29 -0700

    Add tooltip

--- src/components/UserIcon/UserIcon.jsx
+++ src/components/UserIcon/UserIcon.jsx
@@ -1,5 +1,7 @@
 import { Link } from 'react-router-dom';
 import styles from './usericon.module.css';
+import { Tooltip } from '@mui/material';
+import '@emotion/styled';
 
 function UserIcon(props) {
   const { user } = props;
@@ -8,13 +10,16 @@ function UserIcon(props) {
   return (
     <div>
       <button className={styles.user_icon_container}>
-        <Link to={'/users/' + user._id}>
-          <img
-            src={user.profilePhoto}
-            className={styles.user_icon}
-            alt="user avatar"
-          />
-        </Link>
+        <Tooltip title="Profile">
+          <Link to={'/users/' + user._id}>
+            <img
+              src={user.profilePhoto}
+              className={styles.user_icon}
+              alt="user avatar"
+            />
+          </Link>
+        </Tooltip>
+
         <a className={styles.user_link} href={`/users/${user._id}`}>
           {user.username}
         </a>
