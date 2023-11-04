7d652cad763fd9ce63d0e93be54a86c16a9f6685
mrwindupbirdesq <nmaahs11@gmail.com>
2023-11-03 17:07:47 -0700

    Create jsx to list friends

--- src/components/FriendsList/FriendsList.jsx
+++ src/components/FriendsList/FriendsList.jsx
@@ -1,10 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import styles from './friendslist.module.css';
+import UserIcon from '../UserIcon/UserIcon';
 
 function FriendsList() {
+  const [friends, setFriends] = useState([]);
+  //USE EFFECT TO FETCH FRIENDS
   return (
     <div className={styles.container}>
       <h3 className={styles.friendsTitle}>Contacts</h3>
+      {friends && (
+        <div className={styles.friendsList}>
+          {friends.map((friend) => (
+            <UserIcon user={friend} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
