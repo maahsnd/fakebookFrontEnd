885a81a621850e57826c1991ed95e833702e90f7
mrwindupbirdesq <nmaahs11@gmail.com>
2023-11-10 11:36:19 -0800

    Prevent return until props Destructure props rather than assign.

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -6,8 +6,7 @@ import '@emotion/styled';
 import { useNavigate } from 'react-router-dom';
 import FriendRequests from '../FriendRequests/FriendRequests';
 
-function Nav(props) {
-  const { user, setUpdateUser } = props;
+function Nav({ user, setUpdateUser }) {
   const [friendModalIsOpen, setFriendModalIsOpen] = useState(false);
   const navigate = useNavigate();
   const logOut = async () => {
@@ -18,56 +17,57 @@ function Nav(props) {
       navigate('/login');
     }
   };
-  return (
-    <nav className={styles.container}>
-      <button className={styles.homeBtn}>
-        <a href={'/' + user._id}>Fakebook</a>
-      </button>
-      <div className={styles.buttonsContainer}>
-        <UserIcon user={user} />
-        <Tooltip title="Friend Requests">
-          <button
-            className={styles.friendRequestBtn}
-            onClick={() => setFriendModalIsOpen(true)}
-          >
-            <img
-              className={styles.friendRequestImg}
-              src="https://res.cloudinary.com/dscsiijis/image/upload/c_thumb,w_200,g_face/v1699037440/friendship_e6zrqh.jpg"
-              alt="friends img"
-            />
-          </button>
-        </Tooltip>
-        <FriendRequests
-          friendModalIsOpen={friendModalIsOpen}
-          setFriendModalIsOpen={setFriendModalIsOpen}
-          friendRequests={user.friendRequests}
-          setUpdateUser={setUpdateUser}
-          id={user._id}
-        />
-        <Tooltip title="Settings">
-          <button
-            className={styles.settingsBtn}
-            onClick={() => navigate(`/${user._id}/settings`)}
-          >
-            <img
-              className={styles.settingsImg}
-              src="https://res.cloudinary.com/dscsiijis/image/upload/c_scale,w_50/v1699554218/setting_b29rfh.png"
-              alt="Settings icon"
-            />
-          </button>
-        </Tooltip>
-        <Tooltip title="Log Out">
-          <button className={styles.logOutBtn} onClick={logOut}>
-            <img
-              className={styles.logOutImg}
-              src="https://res.cloudinary.com/dscsiijis/image/upload/c_thumb,w_200,g_face/v1699052679/door_ye5pg7.png"
-              alt="log out"
-            />
-          </button>
-        </Tooltip>
-      </div>
-    </nav>
-  );
+  if (user)
+    return (
+      <nav className={styles.container}>
+        <button className={styles.homeBtn}>
+          <a href={'/' + user._id}>Fakebook</a>
+        </button>
+        <div className={styles.buttonsContainer}>
+          <UserIcon user={user} />
+          <Tooltip title="Friend Requests">
+            <button
+              className={styles.friendRequestBtn}
+              onClick={() => setFriendModalIsOpen(true)}
+            >
+              <img
+                className={styles.friendRequestImg}
+                src="https://res.cloudinary.com/dscsiijis/image/upload/c_thumb,w_200,g_face/v1699037440/friendship_e6zrqh.jpg"
+                alt="friends img"
+              />
+            </button>
+          </Tooltip>
+          <FriendRequests
+            friendModalIsOpen={friendModalIsOpen}
+            setFriendModalIsOpen={setFriendModalIsOpen}
+            friendRequests={user.friendRequests}
+            setUpdateUser={setUpdateUser}
+            id={user._id}
+          />
+          <Tooltip title="Settings">
+            <button
+              className={styles.settingsBtn}
+              onClick={() => navigate(`/${user._id}/settings`)}
+            >
+              <img
+                className={styles.settingsImg}
+                src="https://res.cloudinary.com/dscsiijis/image/upload/c_scale,w_50/v1699554218/setting_b29rfh.png"
+                alt="Settings icon"
+              />
+            </button>
+          </Tooltip>
+          <Tooltip title="Log Out">
+            <button className={styles.logOutBtn} onClick={logOut}>
+              <img
+                className={styles.logOutImg}
+                src="https://res.cloudinary.com/dscsiijis/image/upload/c_thumb,w_200,g_face/v1699052679/door_ye5pg7.png"
+                alt="log out"
+              />
+            </button>
+          </Tooltip>
+        </div>
+      </nav>
+    );
 }
 
 export default Nav;
