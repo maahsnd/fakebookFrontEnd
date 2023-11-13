b21c65f7f2ea671182a0ccdaa43f54e315b7e922
mrwindupbirdesq <nmaahs11@gmail.com>
2023-11-13 11:00:54 -0800

    Remove token check Post never rendered independently, check is superfluous.

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -118,10 +118,6 @@ function Post({ post, setError, setUpdatePosts }) {
     </form>
   );
 
-  if (!token || token == 'undefined' || token == null) {
-    navigate('/');
-  }
-
   return (
     <div className={styles.masterContainer}>
       <LikeDisplay
