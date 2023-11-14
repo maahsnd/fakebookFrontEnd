373eaf842daa62742d01a8fe217aa6126b85b158
mrwindupbirdesq <nmaahs11@gmail.com>
2023-11-14 12:43:08 -0800

    Remove effect With id stored in cookies, fetch no longer necessary.

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,41 +1,18 @@
-import React, { useEffect, useState } from 'react';
 import Timeline from '../Timeline/Timeline';
 import Cookies from 'js-cookie';
 import { useNavigate } from 'react-router-dom';
 
 function Home() {
   const userId = Cookies.get('user_id');
-  const [user, setUser] = useState(null);
-  const [updateUser, setUpdateUser] = useState(false);
+
   const token = Cookies.get('jwt_token');
   const navigate = useNavigate();
-  useEffect(() => {
-    const fetchUser = async () => {
-      const response = await fetch('https://localhost:3000/users/' + userId, {
-        headers: {
-          Authorization: 'Bearer ' + token
-        }
-      });
-      const data = await response.json();
-      setUser(data);
-      setUpdateUser(false);
-    };
-    fetchUser();
-  }, [updateUser]);
 
   if (!token || token == 'undefined' || token == null) {
     navigate('/login');
   }
 
-  return (
-    <div>
-      {user && (
-        <>
-          <Timeline id={user._id} />
-        </>
-      )}{' '}
-    </div>
-  );
+  return <Timeline id={userId} />;
 }
 
 export default Home;
