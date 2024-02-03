f7dd595974af14616119819be6555c0c4f9e8ba4
mrwindupbirdesq <nmaahs11@gmail.com>
2024-02-03 10:37:06 -0800

    Bypass login Route directly to guest user home.

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,15 +1,51 @@
 import React, { useState } from 'react';
 import styles from './login.module.css';
 import LoginForm from '../LoginForm/LoginForm';
+import Cookies from 'js-cookie';
+import { useNavigate } from 'react-router-dom';
 function LogIn() {
   const [showSignUpForm, setShowSignUpForm] = useState(false);
   const [slideUp, setSlideUp] = useState(false);
+  const navigate = useNavigate();
 
-  const handleButtonClick = () => {
+  const handleButtonClick = (e) => {
     setSlideUp(true);
-    setTimeout(() => {
+    /*     setTimeout(() => {
       setShowSignUpForm(true);
-    }, '500');
+    }, '500'); */
+    guestSignIn(e);
+  };
+
+  const guestSignIn = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch(
+        'https://fakebookapi-production.up.railway.app/login/guest',
+        {
+          method: 'POST'
+        }
+      );
+      const data = await response.json();
+      if (response.ok) {
+        // Store the JWT token in cookies
+        Cookies.set('jwt_token', data.token, {
+          sameSite: 'strict',
+          secure: true
+        });
+        Cookies.set('user_id', data.userId, {
+          sameSite: 'strict',
+          secure: true
+        });
+        navigate('/home');
+        return;
+      } else {
+        setError(data.msg);
+        // Handle authentication error
+        console.error('Authentication failed');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
   };
 
   return (
